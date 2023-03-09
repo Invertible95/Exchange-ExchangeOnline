7a3362e6d5d86f75e696d63051c00a8ddf63e7b9
Invertible95 <91550933+Invertible95@users.noreply.github.com>
2023-03-09 09:49:25 +0100

    Add files via upload

---
+++ Create Shared Mailbox EXO .ps1
@@ -0,0 +1,29 @@
+
+
+# Connect to Exchange on-premises
+$ExchangeServer = "EXCSRV004.falkadm.se"
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://$ExchangeServer/powershell -Authentication Kerberos -Credential (Get-Credential)
+Import-PSSession $Session -DisableNameChecking -AllowClobber -ErrorAction Stop
+
+# Set the shared mailbox properties
+$SharedMailboxName = Read-Host "Enter the name of the shared mailbox"
+$SharedMailboxAlias = Read-Host "Enter the alias of the shared mailbox" # Can not contain spaces or Nordic letters
+$SharedMailboxUPN = Read-Host "Enter the mailadress of the shared mailbox"
+$SharedMailboxRemoteRoutingAddress = "$SharedMailboxAlias@falkenbergskommun.mail.onmicrosoft.com"
+$SharedMailboxOrganizationalUnit = "OU=Users,OU=Resources,OU=Data,DC=falkadm,DC=se"
+
+$SharedMailbox = @{
+    Name = $SharedMailboxName
+    Alias = $SharedMailboxAlias
+    UserPrincipalName = $SharedMailboxUPN
+    RemoteRoutingAddress = $SharedMailboxRemoteRoutingAddress
+    PrimarySmtpAddress = $SharedMailboxUPN
+    Shared = $true
+    OnPremisesOrganizationalUnit = $SharedMailboxOrganizationalUnit
+}
+
+# Create the shared mailbox on-premises
+New-RemoteMailbox @SharedMailbox | fl Name, UserPrincipalName, RemoteRoutingAddress
+
+# Disconnect from Exchange on-premises
+Remove-PSSession $Session
