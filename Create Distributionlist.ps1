1e30f0ea07fb5587736699037a1e20d72d527230
Invertible95 <91550933+Invertible95@users.noreply.github.com>
2023-03-31 11:33:02 +0200

    function update2

--- Create Distributionlist.ps1
+++ Create Distributionlist.ps1
@@ -10,33 +10,36 @@ $DistGroup = $Name
  
 function ConnectEXO {
     # Exchange connect
- $ExchangeServer = "Enter name of your exchange server"
- $Credentials = (Get-Credential)
- $ExchangeSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://$ExchangeServer/PowerShell/ -Authentication Kerberos -Credential $Credentials    
- Import-PSSession $ExchangeSession -DisableNameChecking -AllowClobber -ErrorAction Stop
- Write-Host "Imported Exchange Session"
+    $ExchangeServer = "Enter name of your exchange server"
+    $Credentials = (Get-Credential)
+    $ExchangeSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://$ExchangeServer/PowerShell/ -Authentication Kerberos -Credential $Credentials    
+    Import-PSSession $ExchangeSession -DisableNameChecking -AllowClobber -ErrorAction Stop
+    Write-Host "Imported Exchange Session"
     
 }
 ConnectEXO
 
-# If a group with this name already exists the script will stop. If the script stops, please doublecheck your AD for any unexpected error group
- try {
-    $NewDistGroup = @{
-        Name = $Name
-        Type = $Type
-        ManagedBy = $ManagedBy
-        PrimarySmtpAddress = $PrimarySmtpAddress
+function CreateDistGroup {
+    # If a group with this name already exists the script will stop. If the script stops, please doublecheck your AD for any unexpected error group
+    try {
+        $NewDistGroup = @{
+            Name               = $Name
+            Type               = $Type
+            ManagedBy          = $ManagedBy
+            PrimarySmtpAddress = $PrimarySmtpAddress
         }
-}
-finally {
-    Write-Host 'Distribution group processing...'
-}
-New-DistributionGroup @NewDistGroup -ErrorAction stop | fl Name, email
-Write-Host "Group created"
+    }
+    finally {
+        Write-Host 'Distribution group processing...'
+    }
+    New-DistributionGroup @NewDistGroup -ErrorAction stop | fl Name, email
+    Write-Host "Group created"
 
-# Only if your organization is using Active Roles, if you're using regular ADUC, scip this
-# Modifies ARS QAD Attribute to allow managers to add and remove members
-Set-QADObject $DistGroup -ObjectAttributes @{edsaManagerCanUpdateMembershipList = $true}
+    # Only if your organization is using Active Roles, if you're using regular ADUC, scip this
+    # Modifies ARS QAD Attribute to allow managers to add and remove members
+    Set-QADObject $DistGroup -ObjectAttributes @{edsaManagerCanUpdateMembershipList = $true}
+}
+CreateDistGroup
 
 # Clean up
 Remove-PSSession $ExchangeSession -Confirm:$false
