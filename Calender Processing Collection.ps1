b27d51d5d71d48365e964abf04d27900c38ae70a
Invertible95 <91550933+Invertible95@users.noreply.github.com>
2023-04-13 08:09:18 +0200

    Change, Calender Proccessing Collection

--- Calender Processing Collection.ps1
+++ Calender Processing Collection.ps1
@@ -1,4 +1,10 @@
 
 ##Set CalenderProcessing collection
 
-Set-CalendarProcessing -Identity "SolhagaSamlingslokalC@falkenberg.se" -AllRequestInPolicy $true
+# "First alternative, These users can submit a request for owner approval if the resource is available."
+Set-CalendarProcessing -Identity "Resource UPN" -AllRequestInPolicy $true/$false
+
+# "Second alternative, These users can submit a request for owner approval if the resource is available."
+# Removes list of Users for approval and auto accept if resource is available and not in conflict with policies 
+Set-CalendarProcessing -Identity "Resource UPN" -RequestInPolicy $true/$false
+
