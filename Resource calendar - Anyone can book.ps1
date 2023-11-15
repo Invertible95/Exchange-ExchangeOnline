ea602b6ff6ec3517699decf5c9f16cab4bab7088
Victor Uhrberg <victor.uhrberg@advania.com>
2023-11-15 13:04:10 +0100

    Added description and notes to explain

--- Resource calendar - Anyone can book.ps1
+++ Resource calendar - Anyone can book.ps1
@@ -1,19 +1,25 @@
+# Script Description:
+# This PowerShell script sets calendar processing properties for a specified room or equipment resource.
+# The script prompts the user to enter the name of the room or equipment, and then applies the specified
+# calendar processing settings using the Set-CalendarProcessing cmdlet.
 
+# Notes:
+# - The 'AutomateProcessing' property is set to 'AutoAccept' to enable automatic processing of meeting requests.
+# - 'AllBookInPolicy' is set to $true to allow all meeting requests to be automatically accepted.
+# - 'AllRequestInPolicy' and 'AllRequestOutOfPolicy' are set to $false to restrict the types of requests processed.
+# - 'ResourceDelegates', 'BookInPolicy', 'RequestInPolicy', and 'RequestOutOfPolicy' are set to $null initially.
 
 $Resource = Read-Host "Enter name of room or equipment"
 
 $CalProp = @{
     AutomateProcessing    = 'AutoAccept'
-
     AllBookInPolicy       = $true
     AllRequestInPolicy    = $false
     AllRequestOutOfPolicy = $false
-
     ResourceDelegates     = $null
     BookInPolicy          = $null
     RequestInPolicy       = $null
     RequestOutOfPolicy    = $null
-
 }
 
 Set-CalendarProcessing $Resource @CalProp
